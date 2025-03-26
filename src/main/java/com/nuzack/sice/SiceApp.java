95647773e017e52f07bb01919c979a1cd36156b3
Jose Zacarias <elzackarias@icloud.com>
2025-03-26 15:37:30 -0600

    Initial

---
+++ src/main/java/com/nuzack/sice/SiceApp.java
@@ -0,0 +1,32 @@
+package com.nuzack.sice;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SiceApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SiceApp.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setMinWidth(921.6);
+        stage.setMinHeight(576);
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(screenBounds.getMinX());
+        stage.setY(screenBounds.getMinY());
+        stage.setWidth(screenBounds.getWidth());
+        stage.setHeight(screenBounds.getHeight());
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
